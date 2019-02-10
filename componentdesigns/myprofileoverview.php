feec7aa0bfe9e26cd999ad620c1a250b498e9a8f
Danny <dannyseymour1990@gmail.com>
2019-02-10 16:13:58 -0500

    about to convert files to php

--- componentdesigns/myprofileoverview.php
+++ componentdesigns/myprofileoverview.php
@@ -9,7 +9,6 @@
       name="viewport"
       content="width=device-width, initial-scale=1, shrink-to-fit=no"
     />
-    <link rel="import" href="./navbar.html" />
 
     <link
       rel="stylesheet"
@@ -20,65 +19,7 @@
   </head>
 
   <body>
-    <nav class="navbar navbar-expand-lg  navbar-light bg-light">
-      <a class="navbar-brand" href="#" style="margin-right: 50vw">
-        <img
-          src="../public/skull.svg"
-          width="30"
-          height="30"
-          class="d-inline-block align-top rounded-circle"
-          alt=""
-        />
-        MernSkeleton
-      </a>
-
-      <a class="navbar-brand " href="#">
-        <img
-          src="../public/danny.jpg"
-          width="30"
-          height="30"
-          class="d-inline-block align-top rounded-circle"
-          alt=""
-        />
-        Danny Seymour
-      </a>
-      <hr />
-      <a class="nav-item nav-link">Home</a>
-      <hr />
-      <button
-        class="navbar-toggler"
-        type="button"
-        data-toggle="collapse"
-        data-target="#navbarNav"
-        aria-controls="navbarNav"
-        aria-expanded="false"
-        aria-label="Toggle navigation"
-      >
-        <span class="navbar-toggler-icon"></span>
-      </button>
-      <div class="collapse navbar-collapse" id="navbarNav">
-        <ul class="navbar-nav mr-auto">
-          <li class="nav-item dropdown">
-            <a
-              class="nav-link dropdown-toggle"
-              href="#"
-              id="navbarDropdown"
-              role="button"
-              data-toggle="dropdown"
-              aria-haspopup="true"
-              aria-expanded="false"
-            >
-            </a>
-            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
-              <a class="dropdown-item" href="#">Edit Profile</a>
-              <a class="dropdown-item" href="#">User Settings</a>
-              <div class="dropdown-divider"></div>
-              <a class="dropdown-item" href="#">Log out</a>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
+  <?php require'./navbar_loggedin';?>
 
     <div class="container">
       <div class="row">
---
+++ componentdesigns/navbar_loggedin.php
@@ -0,0 +1,65 @@
+<nav class="navbar navbar-expand-lg  navbar-light bg-light">
+      <a class="navbar-brand" href="#" style="margin-right: 50vw">
+        <img
+          src="../public/skull.svg"
+          width="30"
+          height="30"
+          class="d-inline-block align-top rounded-circle"
+          alt=""
+        />
+        MernSkeleton
+      </a>
+
+      <a class="navbar-brand " href="#">
+        <img
+          src="../public/danny.jpg"
+          width="30"
+          height="30"
+          class="d-inline-block align-top rounded-circle"
+          alt=""
+        />
+        Danny Seymour
+      </a>
+      <hr />
+      <a class="nav-item nav-link">Home</a>
+      <hr />
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-toggle="collapse"
+        data-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav mr-auto">
+          <li class="nav-item dropdown">
+            <a
+              class="nav-link dropdown-toggle"
+              href="#"
+              id="navbarDropdown"
+              role="button"
+              data-toggle="dropdown"
+              aria-haspopup="true"
+              aria-expanded="false"
+            >
+            </a>
+            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <a class="dropdown-item" href="#"
+                >Put Display name here <br />
+                @ Put Handle Here
+                <div class="dropdown-divider"></div>
+              </a>
+              <a class="dropdown-item" href="#">Profile</a>
+              <a class="dropdown-item" href="#">User Settings</a>
+              <a class="dropdown-item" href="#">Help</a>
+              <div class="dropdown-divider"></div>
+              <a class="dropdown-item" href="#">Log out</a>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </nav>
